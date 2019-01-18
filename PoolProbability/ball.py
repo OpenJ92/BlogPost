e89d78670000c75972646c53fa0f4eb8d181e04e
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-01-18 18:38:43 -0500

    visualization completed

--- PoolProbability/ball.py
+++
@@ -1,4 +0,0 @@
-class ball():
-    def __init__(self, name, location):
-        self.name_ = name
-        self.location_ = location
--- PoolProbability/pocket.py
+++
@@ -1,3 +0,0 @@
-class pocket():
-    def __init__(self):
-        pass
